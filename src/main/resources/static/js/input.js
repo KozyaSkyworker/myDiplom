1ca2912d86d751d040716e2ccdd02e701932031a
KozyaSkyworker <d_koz@inbox.ru>
2023-04-05 16:20:19 +0400

    empty search input disable btn

--- src/main/resources/static/js/input.js
+++ src/main/resources/static/js/input.js
@@ -1,8 +1,10 @@
 const input = document.querySelector('.search__form-input');
 const clear = document.querySelector('.search__form-clear');
+const btn = document.querySelector('.search__form-btn');
 
 input.addEventListener("input", (e) => {
     input.setAttribute("value", e.target.value);
+    btn.removeAttribute("disabled");
     clear.classList.remove('none')
     if(e.target.value == "")
         clear.classList.add('none')
